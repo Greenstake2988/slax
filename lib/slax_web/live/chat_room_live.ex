7d72e46eefbf4ca41e23aaa349ff8ae1239de90f
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2024-06-27 09:45:37 -0600

    add refs to links@

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -52,7 +52,7 @@ defmodule SlaxWeb.ChatRoomLive do
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href="#"
+      href={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
@@ -66,9 +66,18 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
     rooms = Repo.all(Room)
-    room = List.first(rooms)
+
+    room =
+      case Map.fetch(params, "id") do
+        {:ok, id} ->
+          Repo.get!(Room, id)
+
+        :error ->
+          List.first(rooms)
+      end
+
     {:ok, assign(socket, room: room, rooms: rooms, hide_topic?: false)}
   end
 end
