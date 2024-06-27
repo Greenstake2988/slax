d1db614a2fbe7810712e648f7b49639b732935a2
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2024-06-27 10:48:44 -0600

    use patch instead of navigate

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -47,37 +47,41 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp room_link(assigns) do
     ~H"""
-    <a
+    <.link
       class={[
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href={~p"/rooms/#{@room}"}
+      patch={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
         <%= @room.name %>
       </span>
-    </a>
+    </.link>
     """
   end
 
-  def handle_event("toggle-topic", _unsigned_params, socket) do
-    {:noreply, update(socket, :hide_topic?, &(!&1))}
-  end
-
-  def mount(params, _session, socket) do
-    rooms = Repo.all(Room)
-
+  def handle_params(params, _session, socket) do
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
           Repo.get!(Room, id)
 
         :error ->
-          List.first(rooms)
+          List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, room: room, rooms: rooms, hide_topic?: false)}
+    {:noreply, assign(socket, hide_topic?: false, room: room)}
+  end
+
+  def handle_event("toggle-topic", _params, socket) do
+    {:noreply, update(socket, :hide_topic?, &(!&1))}
+  end
+
+  def mount(_params, _session, socket) do
+    rooms = Repo.all(Room)
+
+    {:ok, assign(socket, rooms: rooms)}
   end
 end
